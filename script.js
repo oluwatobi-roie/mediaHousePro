8cf637f7624c38e6af92c4d9f5a9dc85e05cf467
Oluwatobi Akoms <akomolafevicto@gmail.com>
2023-09-28 16:15:38 +0100

    Landing page for all  views

---
+++ script.js
@@ -0,0 +1,10 @@
+function navigation() {
+ var y = document.getElementsByClassName("toggle_icon");
+  var x = document.getElementById("mhpLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    // y.
+  } else {
+    x.style.display = "block";
+  }
+}
