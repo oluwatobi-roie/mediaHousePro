fd15c51fe38c621703c27810d0a6a39e5641c8df
Oluwatobi Akoms <akomolafevicto@gmail.com>
2023-09-29 20:44:54 +0100

    portfolio section for mobile with javasript

--- special.js
+++ special.js
@@ -1,15 +1,3 @@
-function navigation() {
- var y = document.getElementsByClassName("toggle_icon");
-  var x = document.getElementById("mhpLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-    // y.
-  } else {
-    x.style.display = "block";
-  }
-}
-
-// script to initiate slides on small screen
 let slideIndex = 0;
 showSlides();
 
